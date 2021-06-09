d705aa793653f104f91b3624356afcd78b71b3c4
s.stojanovic <simon.stojanovic@salesagility.com>
2021-06-09 12:41:22 +0100

    Require AntiMalwareTrait
    Fix installer namespace error

--- include/upload_file.php
+++ include/upload_file.php
@@ -42,4 +42,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
+require_once "lib/Utility/AntiMalware/AntiMalwareTrait.php";
 require_once "include/UploadFile.php";
