424d06a41585c7993d759dae7fcbb92ffdb99a6c
Jack Anderson <jack.anderson@salesagility.com>
2023-09-07 13:32:44 +0100

    Fix #10153 - Users creating with incorrect password set

--- modules/Users/User.php
+++ modules/Users/User.php
@@ -679,9 +679,8 @@ class User extends Person implements EmailInterface
 
         if (!$this->verify_data()) {
             SugarApplication::appendErrorMessage($this->error_string);
-            return SugarApplication::redirect('Location: index.php?action=Error&module=Users');
+            return SugarApplication::redirect('index.php?action=Error&module=Users');
         }
-
 
         $retId = parent::save($check_notify);
         if (!$retId) {
@@ -707,11 +706,11 @@ class User extends Person implements EmailInterface
             if (!$this->change_password($_POST['old_password'], $_POST['new_password'])) {
                 if (isset($_POST['page']) && $_POST['page'] === 'EditView') {
                     SugarApplication::appendErrorMessage($this->error_string);
-                    SugarApplication::redirect("Location: index.php?action=EditView&module=Users&record=" . $_POST['record']);
+                    SugarApplication::redirect("index.php?action=EditView&module=Users&record=" . $retId);
                 }
                 if (isset($_POST['page']) && $_POST['page'] === 'Change') {
                     SugarApplication::appendErrorMessage($this->error_string);
-                    SugarApplication::redirect("Location: index.php?action=ChangePassword&module=Users&record=" . $_POST['record']);
+                    SugarApplication::redirect("index.php?action=ChangePassword&module=Users&record=" . $retId);
                 }
             }
         }
